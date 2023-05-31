4e54c8d2b1d0fd185b81e6b965963f597a718845
Smartmotion-LFSM <123175878+Smartmotion-LFSM@users.noreply.github.com>
2023-05-31 09:40:12 +0200

    Update main.py

--- SmartMotion/Programmcode/main.py
+++ SmartMotion/Programmcode/main.py
@@ -13,6 +13,8 @@ RED_LOWER = (0, 70, 50)
 RED_UPPER = (10, 255, 255)
 GREEN_LOWER = (40, 70, 50)
 GREEN_UPPER = (80, 255, 255)
+YELLOW_LOWER = (20, 70, 50)
+YELLOW_UPPER = (40, 255, 255)
 
 # GPIO-Pins für den Ultraschallsensor
 TRIGGER_PIN = 23
@@ -33,10 +35,12 @@ MOTOR_PIN = 16
 BLUE_FREQ = 500
 ORANGE_FREQ = 1000
 
+
 # Funktion zum Ein- und Ausschalten des Relais
 def toggle_relay(state):
     GPIO.output(RELAY_PIN, state)
-    
+
+
 # Funktion zum Drehen des Steppermotors nach links
 def rotate_left():
     global rotate_counter
@@ -44,6 +48,7 @@ def rotate_left():
     time.sleep(3)
     p.ChangeDutyCycle(7.5)
     rotate_counter += 1
+
 
 # Funktion zum Drehen des Steppermotors nach rechts
 def rotate_right():
@@ -53,11 +58,13 @@ def rotate_right():
     p.ChangeDutyCycle(7.5)
     rotate_counter += 1
 
+
 # Funktion zum Ausweichen nach links
 def escape_left():
     p.ChangeDutyCycle(5)
     time.sleep(0.5)
     p.ChangeDutyCycle(7.5)
+
 
 # Funktion zum Asuweichen nach rechts
 def escape_right():
@@ -65,13 +72,15 @@ def escape_right():
     time.sleep(0.5)
     p.ChangeDutyCycle(7.5)
 
+
 # Funktion zur Verarbeitung der Konturen
 def process_contours(contours, frame, color):
     for contour in contours:
         area = cv2.contourArea(contour)
         if area > MIN_CONTOUR_AREA:
             x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
 
 # Funktion zur Distanzmessung mit dem Ultraschallsensor
 def measure_distance():
@@ -93,6 +102,7 @@ def measure_distance():
 
     return distance
 
+
 # Funktion zur Farberkennung mit dem TCS3200 Farbsensor
 def detect_color():
     GPIO.output(S2_PIN, GPIO.LOW)
@@ -112,6 +122,7 @@ def detect_color():
     else:
         return 0
 
+
 def measure_frequency():
     frequency = detect_color()
 
@@ -125,10 +136,11 @@ def measure_frequency():
     else:
         return "Unbekannt"
 
+
 # Initialisierung der GPIO-Pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
-toggle_relay(GPIO.HIGH)  # Relais anschalten
+toggle_relay(GPIO.LOW)  # Relais anschalten
 
 # Initialisieren des Servos
 GPIO.setup(MOTOR_PIN, GPIO.OUT)
@@ -153,6 +165,7 @@ cam = cv2.VideoCapture(0)
 
 # Initialisierung der Variablen
 rotate_counter = 0
+yellow_detected = False
 
 while True:
     ret, frame = cam.read()
@@ -164,9 +177,10 @@ while True:
     # Umwandeln des Frames in den HSV-Farbraum
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Erkennung der roten und grünen Farbe
+    # Erkennung der roten, grünen und gelben Farbe
     red_mask = cv2.inRange(hsv, RED_LOWER, RED_UPPER)
     green_mask = cv2.inRange(hsv, GREEN_LOWER, GREEN_UPPER)
+    yellow_mask = cv2.inRange(hsv, YELLOW_LOWER, YELLOW_UPPER)
 
     # Erosion und Dilatation, um das Rauschen zu reduzieren
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
@@ -174,14 +188,18 @@ while True:
     red_mask = cv2.dilate(red_mask, kernel, iterations=2)
     green_mask = cv2.erode(green_mask, kernel, iterations=2)
     green_mask = cv2.dilate(green_mask, kernel, iterations=2)
+    yellow_mask = cv2.erode(yellow_mask, kernel, iterations=2)
+    yellow_mask = cv2.dilate(yellow_mask, kernel, iterations=2)
 
     # Konturerkennung
     red_contours, _ = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     green_contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    yellow_contours, _ = cv2.findContours(yellow_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Verarbeitung der Konturen
     process_contours(red_contours, frame, (0, 0, 255))  # Rahmen um rote Konturen in Rot zeichnen
     process_contours(green_contours, frame, (0, 255, 0))  # Rahmen um grüne Konturen in Grün zeichnen
+    process_contours(yellow_contours, frame, (0, 255, 255))  # Rahmen um gelbe Konturen in Gelb zeichnen
 
     # Distanzmessung
     distance = measure_distance()
@@ -196,15 +214,25 @@ while True:
     quarter_width = int(width / 4)
 
     # Überprüfung, ob ein rotes oder grünes Objekt im rechten Viertel ist und die Distanz kleiner als 30 cm ist
-    if any([x < quarter_width for x, _, _, _ in cv2.boundingRect(contour) for contour in red_contours]) and distance < 30:
+    if any([x < quarter_width for x, _, _, _ in cv2.boundingRect(contour) for contour in
+            red_contours]) and distance < 30:
         escape_right()
         cv2.putText(frame, "Nach rechts ausweichen", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    elif any([x < quarter_width for x, _, _, _ in cv2.boundingRect(contour) for contour in green_contours]) and distance < 30:
+    elif any([x < quarter_width for x, _, _, _ in cv2.boundingRect(contour) for contour in
+              green_contours]) and distance < 30:
         escape_left()
         cv2.putText(frame, "Nach links ausweichen", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    elif any([x < quarter_width for x, _, _, _ in cv2.boundingRect(contour) for contour in
+              yellow_contours]) and distance < 30:
+        yellow_detected = True
+        cv2.putText(frame, "Gelbes Objekt erkannt", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        toggle_relay(GPIO.HIGH)
     else:
         cv2.putText(frame, "Geradeaus fahren", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
+    # Zeichnen eines gelben Rahmens, wenn ein gelbes Objekt erkannt wurde
+    if yellow_detected:
+        cv2.rectangle(frame, (0, 0), (width, height), (0, 255, 255), 2)
 
     # Anzeige des Frames
     cv2.imshow("Objekterkennung", frame)
@@ -214,6 +242,8 @@ while True:
         break
 
 # Freigeben der Ressourcen
+GPIO.output(RELAY_PIN, GPIO.LOW)  # Relais ausschalten
+toggle_relay(GPIO.LOW)
 cam.release()
 cv2.destroyAllWindows()
 GPIO.cleanup()
